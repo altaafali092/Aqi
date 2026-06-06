9169885f3e0786d958e998721728e9507d0ed5e6
altaafali092 <altaafali092@gmail.com>
2026-06-06 17:44:25 +0545

    feat: create IotReading model with fillable attributes and type casting

--- app/Models/IotReading.php
+++ app/Models/IotReading.php
@@ -8,14 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class IotReading extends Model
 {
     use HasFactory;
-
     protected $fillable = [
-        'ward_id', 'pm1_0', 'pm2_5', 'pm10', 'temperature', 'humidity', 'recorded_at',
+        'ward_id',
+        'pm1_0',
+        'pm2_5',
+        'pm10',
+        'temperature',
+        'humidity',
+        'recorded_at'
     ];
 
     protected $casts = [
         'recorded_at' => 'datetime',
-        'pm1_0' => 'float', 'pm2_5' => 'float', 'pm10' => 'float',
-        'temperature' => 'float', 'humidity' => 'float',
+        'pm1_0' => 'float',
+        'pm2_5' => 'float',
+        'pm10' => 'float',
+        'temperature' => 'float',
+        'humidity' => 'float',
     ];
 }
